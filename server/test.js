076066c10dabf83b273569c387b69deaaf339c46
Zach <zach.kysar@gmail.com>
2015-11-11 12:34:43 -0800

    Added potential fix for server

--- server/test.js
+++ server/test.js
@@ -8,22 +8,68 @@ password: 'mysqlpassword',
 database: 'perk'
 });
 my_client.connect();
- 
-var sql = 'select * from users';
- 
-my_client.query(sql, function (err, rows, fields) {
+
+function query(sql){
+		return my_client.query(sql, function (err, rows, fields) {
         if (err) {
                 console.log('can not connect');
                 console.log(err);
                 return;
-        }
- 
-        for (var i in rows) {
-                console.log(rows[i]);
-        }
-});
-my_client.end();
-
-//var x = connection.query('SELECT EXISTS(SELECT 1 from users where email="smashtilldawn.com")');    
-//console.log(x);
-
+			}
+	}
+}
+function disconnect(){
+	my_client.disconnect();
+}
+function addUser(name,email,phone,car){
+	var sql = /*SOMETHING HERE*/;
+	if(!userExists(name,email,phone,car)) query(sql);
+	else console.log("CAN'T ADD: USER " + name + " ALREADY IN users");
+	query(sql);
+}
+function removeUser(userID){
+	var sql = /*SOMETHING HERE*/;
+	if(userExistsInTable("users",userID) query(sql);
+	else console.log("CAN'T DELETE: USER " + userID + " NOT IN users");
+	query(sql);
+}
+function userExists(name,email,phone,car){
+	var sql = /*SOMETHING HERE*/;
+	var result = query(sql);
+	if(result == /*SOMETHING HERE*/) return true;
+	else return false;
+}
+function addPark(userID,lat,lon,parkTime){
+	var sql = /*SOMETHING HERE*/
+	if(!userExistsInTable("park",userID)) query(sql);
+	else console.log("CAN'T ADD: USER " + userID + " ALREADY IN park");
+}
+function removePark(userID){
+	var sql = /*SOMETHING HERE*/;
+	if(userExistsInTable("park",userID)) query(sql);
+	else console.log("CAN'T DELETE: USER " + userID + " NOT IN park");
+}
+function addPickup(userID,lat,lon,time,lot){
+	var sql = /*SOMETHING HERE*/;
+	if(!userExistsInTable("pickup",userID)) query(sql);
+	else console.log("CAN'T ADD: USER " + userID + " ALREADY IN pickup");
+}
+function removePickup(userID){
+	var sql = /*SOMETHING HERE*/;
+	if(userExistsInTable("pickup",userID)) query(sql);
+	else console.log("CAN'T DELETE: USER " + userID + " NOT IN pickup");
+}
+function getPickupList(){
+	var sql = /*SOMETHING HERE*/;
+	return query(sql);
+}
+function getParkList(){
+	var sql = /*SOMETHING HERE*/;
+	return query(sql);
+}
+function userExistsInTable(table,userID){
+	var sql = /*SOMETHING HERE*/;
+	var result = query(sql);
+	if(result == /*SOMETHING HERE*/) return true;
+	else return false;
+}
