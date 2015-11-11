217e4f072520f7fabe3826c63e9c1c0b9f470b94
Zach <zach.kysar@gmail.com>
2015-11-11 12:51:33 -0800

    Added 0s so the script would run.

--- server/server.js
+++ server/server.js
@@ -22,54 +22,54 @@ function disconnect(){
 	my_client.disconnect();
 }
 function addUser(name,email,phone,car){
-	var sql = /*SOMETHING HERE*/;
+	var sql = 0/*SOMETHING HERE*/;
 	if(!userExists(name,email,phone,car)) query(sql);
 	else console.log("CAN'T ADD: USER " + name + " ALREADY IN users");
 	query(sql);
 }
 function removeUser(userID){
-	var sql = /*SOMETHING HERE*/;
+	var sql = 0/*SOMETHING HERE*/;
 	if(userExistsInTable("users",userID) query(sql);
 	else console.log("CAN'T DELETE: USER " + userID + " NOT IN users");
 	query(sql);
 }
 function userExists(name,email,phone,car){
-	var sql = /*SOMETHING HERE*/;
+	var sql = 0/*SOMETHING HERE*/;
 	var result = query(sql);
-	if(result == /*SOMETHING HERE*/) return true;
+	if(result == 0/*SOMETHING HERE*/) return true;
 	else return false;
 }
 function addPark(userID,lat,lon,parkTime){
-	var sql = /*SOMETHING HERE*/
+	var sql = 0/*SOMETHING HERE*/
 	if(!userExistsInTable("park",userID)) query(sql);
 	else console.log("CAN'T ADD: USER " + userID + " ALREADY IN park");
 }
 function removePark(userID){
-	var sql = /*SOMETHING HERE*/;
+	var sql = 0/*SOMETHING HERE*/;
 	if(userExistsInTable("park",userID)) query(sql);
 	else console.log("CAN'T DELETE: USER " + userID + " NOT IN park");
 }
 function addPickup(userID,lat,lon,time,lot){
-	var sql = /*SOMETHING HERE*/;
+	var sql = 0/*SOMETHING HERE*/;
 	if(!userExistsInTable("pickup",userID)) query(sql);
 	else console.log("CAN'T ADD: USER " + userID + " ALREADY IN pickup");
 }
 function removePickup(userID){
-	var sql = /*SOMETHING HERE*/;
+	var sql = 0/*SOMETHING HERE*/;
 	if(userExistsInTable("pickup",userID)) query(sql);
 	else console.log("CAN'T DELETE: USER " + userID + " NOT IN pickup");
 }
 function getPickupList(){
-	var sql = /*SOMETHING HERE*/;
+	var sql = 0/*SOMETHING HERE*/;
 	return query(sql);
 }
 function getParkList(){
-	var sql = /*SOMETHING HERE*/;
+	var sql = 0/*SOMETHING HERE*/;
 	return query(sql);
 }
 function userExistsInTable(table,userID){
-	var sql = /*SOMETHING HERE*/;
+	var sql = 0/*SOMETHING HERE*/;
 	var result = query(sql);
-	if(result == /*SOMETHING HERE*/) return true;
+	if(result == 0/*SOMETHING HERE*/) return true;
 	else return false;
 }
